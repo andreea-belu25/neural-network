391d2a5fceaf4bdf4a9975fef8063df4a7fb4a5a
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 19:23:52 +0300

    Update get_adjanceny_matrix.m

--- get_adjanceny_matrix.m
+++ get_adjanceny_matrix.m
@@ -1,46 +1,60 @@
 function [Adj] = get_adjanceny_matrix(Labyrinth)
   [nr_lines nr_columns] = size(Labyrinth);
   p = nr_columns * nr_lines;
+  
   for i = 1:nr_lines
     for j=1:nr_columns
       number = Labyrinth(i,j);
       nrnou = 0;
       p10 = 1;
+      
        while number != 0
          rest = mod(number, 2);
          nrnou = rest * p10 + nrnou;
          p10 = p10 * 10;
          number = floor(number / 2);
        endwhile
+       
        Labyrinth(i,j) = nrnou;
     endfor
   endfor
+  
   Adj = zeros(p + 2, p + 2);
+  
   for i=1:nr_lines
     for j=1:nr_columns
       number = Labyrinth(i,j);
       b0 = mod(number, 10);
+      
       number = floor(number / 10);
       b1 = mod(number, 10);
+      
       number = floor(number / 10);
       b2 = mod(number, 10);
+      
       number = floor(number / 10);
       b3 = mod(number, 10);
+      
       index_in_matrix = (i - 1) * nr_columns + j;
+      
       if(i != 1 && i != nr_lines && j != 1 && j != nr_columns)
         if(b0 == 0)
           Adj(index_in_matrix, index_in_matrix - 1) = 1;
         endif
+        
         if(b1 == 0)
           Adj(index_in_matrix, index_in_matrix + 1) = 1;
         endif
+        
         if(b2 == 0)
           Adj(index_in_matrix + 1, index_in_matrix) = 1;
         endif
+        
         if(b3 == 0)
           Adj(index_in_matrix - 1, index_in_matrix) = 1;
         endif
       endif
+      
       if(i == 1 && b0 == 0)
           if(j == 1)
               Adj(index_in_matrix, p + 2) = 1;
@@ -48,12 +62,15 @@ function [Adj] = get_adjanceny_matrix(Labyrinth)
               Adj(index_in_matrix, index_in_matrix - 1) = 1;
           endif
       endif
+      
       if(i == 1 && b3 == 0)
           Adj(index_in_matrix, p + 1) = 1;
       endif
+      
       if(i == 1 && b2 == 0)
           Adj(index_in_matrix, i * nr_columns + j) = 1;
       endif
+     
      if(i == 1 && b1 == 0)
           if(j == nr_columns)
               Adj(index_in_matrix, p + 2) = 1;
@@ -61,12 +78,15 @@ function [Adj] = get_adjanceny_matrix(Labyrinth)
               Adj(index_in_matrix, index_in_matrix + 1) = 1;
            endif
       endif
+      
       if(i == nr_lines && b2 == 0)
           Adj(index_in_matrix, p + 1) = 1;
       endif
+      
       if(i == nr_lines && b3 == 0)
           Adj(index_in_matrix, index_in_matrix - nr_columns) = 1;
       endif
+      
       if(i == nr_lines && b0 == 0)
         if(j == 1)
            Adj(index_in_matrix, p + 2) = 1;
@@ -74,6 +94,7 @@ function [Adj] = get_adjanceny_matrix(Labyrinth)
            Adj(index_in_matrix, index_in_matrix - 1) = 1;
         endif
      endif
+     
      if(i == nr_lines && b1 == 0)
         if(j == nr_columns)
           Adj(index_in_matrix, p + 2) = 1;
@@ -81,37 +102,47 @@ function [Adj] = get_adjanceny_matrix(Labyrinth)
           Adj(index_in_matrix, index_in_matrix + 1) = 1;
         endif
      endif
+     
      if(j == 1 && i != 1 && i != nr_lines)
         if(b3 == 0)
           Adj(index_in_matrix, index_in_matrix - nr_columns) = 1;
         endif
+        
         if(b2 == 0)
             Adj(index_in_matrix, index_in_matrix + nr_columns) = 1;
          endif
+         
          if(b0 == 0)
               Adj(index_in_matrix, p + 2) = 1;
          endif
+         
          if(b1 == 0)
             Adj(index_in_matrix, index_in_matrix + 1) = 1;
          endif
       endif
+      
       if(j == nr_columns && i != 1 && i != nr_lines)
           if(b3 == 0)
             Adj(index_in_matrix,index_in_matrix - nr_columns) = 1;
           endif
+          
           if(b0 == 0)
             Adj(index_in_matrix, index_in_matrix - 1) = 1;
           endif
+          
           if(b1 == 0)
             Adj(index_in_matrix, p + 2) = 1;
           endif
+          
           if(b2 == 0)
             Adj(index_in_matrix, index_in_matrix + nr_columns) = 1;
           endif
         endif
       endfor
     endfor
+    
     Adj(p + 1, p + 1) = 1;
     Adj(p + 2, p + 2) = 1;
     Adj = sparse(Adj);
+    
 endfunction
