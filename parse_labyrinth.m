9319db36b0b1e9ffe1338b2cbae8fd1a07c81e01
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 19:26:13 +0300

    Update parse_labyrinth.m

--- parse_labyrinth.m
+++ parse_labyrinth.m
@@ -1,11 +1,15 @@
 function [Labyrinth] = parse_labyrinth(filename)
   fid = fopen(filename, 'r');
   dims = fscanf(fid, '%d', [1,2]);
+  
   nr_lines = dims(1,1);
   nr_columns = dims(1,2);
+  
   for i = 1:nr_lines
     line = fscanf(fid, '%d', [1 nr_columns]);
     Labyrinth(i,:) = line;
   endfor
+  
   fclose(fid);
+  
 endfunction
