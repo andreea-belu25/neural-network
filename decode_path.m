35e26c21fcd8fc1c6d38c66f6db1ea57c1d3442d
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 19:13:22 +0300

    Update decode_path.m

--- decode_path.m
+++ decode_path.m
@@ -1,6 +1,6 @@
 function [decoded_path] = decode_path(path, lines, cols)
-  labyrinth = parse_labyrinth ("Mn_input.txt");  %matricea citita la primul subpunct
-  %pun intr-un vector toate elemnetele matricei
+  labyrinth = parse_labyrinth ("Mn_input.txt");
+ 
   poz = 1;
   for i=1:lines
     for j=1:cols
@@ -8,10 +8,12 @@ function [decoded_path] = decode_path(path, lines, cols)
       poz++;
     endfor
   endfor
-  % parcurg vectorul si daca gasesc duplicate, pe a doua pozitie gasita a aceluiasi numar pun -1
-  % -1 nu se va gasi in matrice pentru ca in matrice sunt elemnetele de la 0 la 15
-  % am facut acest artificiu pentru a-mi afisa prima pozitie a unui numar intalnit in matrice, 
- % neglijandu-le pe restul daca exista duplicate
+  
+  % I traverse the vector and if I find duplicates, on the second position found of the same number I put -1
+  % -1 will not be found in the matrix because the matrix contains elements from 0 to 15
+  % I made this trick to display the first position of a number encountered in the matrix,
+  % ignoring the rest if duplicates exist
+
   for i=1:poz - 1
     for j=1:poz - 1
        if (i != j && v(i) == v(j))
@@ -19,7 +21,9 @@ function [decoded_path] = decode_path(path, lines, cols)
        endif
      endfor
   endfor
-  % mut toate elementele din vector inapoi in matrice
+  
+  % move back all the elements
+  
   poz = 1;
   for i=1:lines
     for j=1:cols
@@ -27,7 +31,9 @@ function [decoded_path] = decode_path(path, lines, cols)
       poz++;
     endfor
   endfor
-  % parcurg matricea si vectorul path si formez vectorul decoded_path
+  
+  % form `decoded_path` array
+  
   [n m] = size(path');
   i1 = 1;
   j1 = 1;
@@ -45,3 +51,4 @@ function [decoded_path] = decode_path(path, lines, cols)
     endfor
   endfor
 endfunction
+
